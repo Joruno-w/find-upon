4d01287e8f7db6abcd55cab83b4bb323a38ea469
Joruno <1710642275@qq.com>
2022-09-01 19:15:51 +0800

    Update index.test.ts

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,5 +2,11 @@ import { expect, it } from "vitest";
 import { findUp } from "../src";
 
 it("finds up file", async () => {
+  expect(await findUp("index.js")).toMatchInlineSnapshot('"/Users/wangshengliang/Desktop/find-upon/index.js"');
+  expect(await findUp("index.js",{type: 'file'})).toMatchInlineSnapshot('"/Users/wangshengliang/Desktop/find-upon/index.js"');
+  expect(await findUp("index.test.js",{type: 'directory'})).toMatchInlineSnapshot('undefined');
+  expect(await findUp("index.js",{type: 'file'})).toMatchInlineSnapshot('"/Users/wangshengliang/Desktop/find-upon/index.js"');
+  expect(await findUp("index.js",{type: 'file'})).toMatchInlineSnapshot('"/Users/wangshengliang/Desktop/find-upon/index.js"');
+  expect(await findUp("index.js",{type: 'file'})).toMatchInlineSnapshot('"/Users/wangshengliang/Desktop/find-upon/index.js"');
   expect(await findUp("index.js",{type: 'file'})).toMatchInlineSnapshot('"/Users/wangshengliang/Desktop/find-upon/index.js"');
 });
